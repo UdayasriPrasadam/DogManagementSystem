57d217f745584b617ce6d28e1db9c174c4c2b2bc
UdayasriPrasadam <111814533+UdayasriPrasadam@users.noreply.github.com>
2023-04-24 21:33:25 -0500

    added files

---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+ * @author Udayasri Prasadam
+ *
+ */
+public interface TrainerRepository extends CrudRepository<Trainer,Integer> {
+	
+
+}
